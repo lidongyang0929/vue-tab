2520eb4b76afd871f46a327724d67476d26a2e81
lidongyang <15650785172@163.com>
2019-03-16 18:12:50 +0800

    1

---
+++ main.js
@@ -0,0 +1,27 @@
+let view = new Vue({
+   el:'#app',
+   data:{
+       selected: 'a',
+       tabs:[{name: 'a',content: 'aaa'},{name:'b',content:'bbb'},{name:'c',content:'ccc'}]
+   },
+   template:`
+   <div>
+       <ol>
+         <li v-for="tab in tabs"
+         v-on:click="selected = tab.name"
+         v-bind:class="{active:selected === tab.name}">{{tab.name}}</li>
+         
+       </ol>
+       <ol>
+        <li v-for="tab in tabs"
+        v-show="selected === tab.name">{{tab.content}}</li>
+       </ol>
+   
+   </div>
+   `
+
+
+
+
+
+})
